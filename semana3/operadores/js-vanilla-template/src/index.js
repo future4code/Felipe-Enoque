38835fb4fe6fb0651364693fd4ca16d5096ff3be
Felipe Enoque <fenoque641@gmail.com>
2020-07-29 16:29:35 -0300

    projeto completo

--- semana3/operadores/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -1,9 +1,93 @@
-//1 a. true false
-// b. true false true
+//Exercicio 1:
+
+//a. true false
+
+//b. true false true
+
 //c. false
+
 //e. booleano
 
-//2 a. undefined
+//Exercicio 2:
+
+//a. undefined
+
 //b. null
+
 //c.  10
-//d. 
+
+//d. 0
+
+//e. 19
+
+//f. i+6 
+
+
+//Exercicios de excrita de codigo:
+//Exercicio 1:
+
+//a.
+//let idade = Number(prompt("Qual a sua idade :"))
+
+//b.
+//let idadm = Number(prompt("Qual a idade do seu melhor amigo(a) :"))
+//let resultid = idade >= idadm
+//let difer = idade - idadm
+
+//c.
+//console.log("Sua idade é maior do que a do seu maior amigo ?")
+//console.log(resultid)
+//console.log("A diferença de idade entre voces é :")
+//console.log(difer)
+//const diferente = number(idade) - Number(idadma)
+
+
+//Exercicio 2:
+
+//a.
+
+//let nump = Number(prompt("Digite um numero pa :"))
+
+//b.
+//let rest = nump%2
+
+//console.log(rest)
+
+//c.
+//o padrão é que dividindo um numero par por dois o resto sempre é 0
+
+//d.
+//se um numero impar for digitado tem um resto sempre 
+
+
+//Exercicio 3:
+
+//a.
+//let listadetarefa = []
+
+//let taref1 = prompt("Digite uma tarefa sua :")
+//let teref2 = prompt("Digite outra tarefa sua :")
+//let taref3 = prompt("Digite mais uma tarefa :")
+
+//listadetarefa.push(taref1)
+//listadetarefa.push(taref2)
+//listadetarefa.push(taref3)
+
+//console.log(taref1)
+//console.log(taref2)
+//console.log(taref3)
+
+
+//Exercicio 4 :
+
+
+let name = prompt("Digite seu nome :")
+let email = prompt("Digite seu email :")
+
+
+console.log("O e-mail " , email , " foi cadastrado com sucesso. Seja bem-vinda(o)," , name)
+
+
+
+
+
