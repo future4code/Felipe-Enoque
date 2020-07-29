3a3f14230f5e9418a0231db11ae39145c6cd2d81
Felipe Enoque <fenoque641@gmail.com>
2020-07-29 15:14:13 -0300

    pronta entrea

--- semana3/variáveis/js-vanilla-template/src/index.js
+++ semana3/variáveis/js-vanilla-template/src/index.js
@@ -1,7 +1,94 @@
-/*1= 10, 5; */
-/*2= 10, 10, 10; */
-const nome
-const idade
 
-console.log(typeof nome)
-console.log(typeof idade)
+//Exercicio 1 = 10, 5; 
+
+//Exercicio 2= 10, 10, 10;
+
+//Exercicios de escrita//
+//Exercicio 1:
+//a. //d.
+//let nome = prompt("Digite seu nome:")
+
+//b. //d.
+//let idade = prompt("Digite sua idade:")
+
+//c.
+//console.log(typeof nome)
+//console.log(typeof idade)
+
+//d.
+//underfined porque não foi atribuido nem um valor a variavel
+
+//e.
+//Mudou para string devido ter sido atribuido um valor
+
+//f.
+//console.log("Olá ", nome, "voce tem ",idade,"anos")
+
+
+//Exercicio 2:
+
+//let corf = prompt("Qual sua cor favorita: ")
+//let ender = prompt("Qual o seu endereço")
+//let filfav = prompt("Qual o seu filme favorito")
+//let cidad = prompt("Qual sua cidade")
+//let aniv = prompt("Quando é o dia do seu aniversario ")
+
+//console.log("Qual a sua cor favorita ?")
+//console.log("Resposta: ",corf)
+
+//console.log("Qual o seu endereço ?")
+//console.log("Resposta: ",ender)
+
+//console.log("Qual o seu filme favorito ?")
+//console.log("Resposta: ",filfav)
+
+//console.log("Qual a sua cidade ?")
+//console.log("Resposta: ",cidad)
+
+//console.log("Qual é o dia do seu aniversario?")
+//console.log("Resposta: ",aniv)
+
+
+
+//Exercicio 3:
+//a.
+//let array = ['Lasanha', 'pipoca', 'cachorro quente', 'macarrao', 'miojo']
+
+//console.log (array[0])
+//console.log (array[1])
+//console.log (array[2])
+//console.log (array[3])
+//console.log (array[4])
+
+
+//b.
+//let array = ['Lasanha', 'pipoca', 'cachorro quente', 'macarrao', 'miojo']
+
+//console.log("Essas são minhas comidas preferidas: ")
+//console.log (array[0])
+//console.log (array[1])
+//console.log (array[2])
+//console.log (array[3])
+//console.log (array[4])
+
+
+//c.
+//let array = ['Lasanha', 'pipoca', 'cachorro quente', 'macarrao', 'miojo']
+//let comepu = prompt("Diga uma comida favorita: ")
+
+//console.log (array[0])
+//console.log (comepu)
+//console.log (array[2])
+//console.log (array[3])
+//console.log (array[4])
+
+
+//Exercicio 4
+
+
+let per = ["Voce esta usando azul ?", "Voce esta bem ?" , "Voce dormil bem ?"]
+let respostas = [false , true , true]
+
+console.log(per[0] , respostas[0])
+console.log(per[1] , respostas[1])
+console.log(per[2] , respostas[2])
